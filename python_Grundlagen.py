1c9a93a4407063eec463d749533d8f357d7ca381
yi-joseph <yimiaoheidelberg@gmail.com>
2025-07-29 10:53:35 +0200

    Create python_Grundlagen.py

---
+++ python_Grundlagen.py
@@ -0,0 +1,10 @@
+# 1 
+# PEP 8 (Python Enhancement Proposal #8) Formating
+# Installierung das Packet black
+# in Powershell: 
+# pip install black
+# Verwendung:
+# python -m black eine_Python_datei.py
+# Z.B. 
+# python -m black bedeutung.py
+# Hier wird die Python-Datei nach PEP 8 formatiert. 
